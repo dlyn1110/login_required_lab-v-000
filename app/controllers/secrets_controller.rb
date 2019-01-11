43a466ef9af2f073d9530fb5bd905eae0ae9f000
Dani Lyn <dlyn1110@gmail.com>
2019-01-11 03:33:37 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -10,5 +10,11 @@ class SecretsController < ActionController::Base
    end
  end
 
+ private
+
+ def require_login
+  redirect_to controller: 'sessions', action: 'new' unless current_user
+ end  
+
 
 end

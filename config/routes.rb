b0d5d244c2a197d759c9201678d83d503abde9fa
Dani Lyn <dlyn1110@gmail.com>
2019-01-11 03:43:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'application#welcome'
- get '/login' => 'sessions#new'
- post '/login' => 'sessions#create'
- post '/logout' => 'sessions#destroy'
- get '/show' => 'secrets#show'
+  resources :sessions
+
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/show' => 'secrets#show'
+    delete '/session' => 'sessions#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

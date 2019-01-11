aacac9d0217d25618ddc6c33f0ff5641795b1905
Dani Lyn <dlyn1110@gmail.com>
2019-01-11 03:51:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-  def current_user 
+  def current_user
     @username ||= session[:name]
-  end  
+  end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,18 +1,18 @@
-class SecretsController < ApplicationController
+class SecretsController < ApplicationController 
     before_action :require_login
     skip_before_action :require_login, only: [:index]
 
-   def show
-    if !current_user
+   def show 
+    if !current_user 
         redirect_to '/login'
-    else
+    else    
         @secret = "The secret message is found here. Another edit here."
-    end
-   end
+    end     
+   end      
 
-   private
+   private 
 
    def require_login
     redirect_to controller: 'sessions', action: 'new' unless current_user
-   end
+   end  
 end 

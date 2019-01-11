c5af1537b27418370547f4a4cc824128df444465
Dani Lyn <dlyn1110@gmail.com>
2019-01-11 03:48:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController 
 
-    def new 
-    end 
-    
-    def create 
+    def new
+    end
+
+    def create
         if params[:name].nil? || params[:name] == ""
             redirect_to controller: 'sessions', action: 'new'
-        else 
+        else
             session[:name] = params[:name]
             redirect_to '/'
-        end         
-    end   
-    
-    def destroy 
-        session.delete :name 
-        redirect_to '/'
-    end     
+        end
+     end
 
-end     
+    def destroy
+        session.delete :name
+        redirect_to '/'
+    end
+
+  end

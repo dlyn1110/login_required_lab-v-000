cbccef1cc79eabeb2c56e7b318494e6d948d0234
Dani Lyn <dlyn1110@gmail.com>
2019-01-11 03:47:54 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
-class SessionsController < ActionController::Base
-  def new
-  end
+class SessionsController < ApplicationController 
 
-  def create
-    if params[:name].nil? || params[:name] == ""
-      redirect_to login_path
-    else
-    session[:name] = params[:name]
-    redirect_to root_path
-  end
-end
+    def new 
+    end 
+    
+    def create 
+        if params[:name].nil? || params[:name] == ""
+            redirect_to controller: 'sessions', action: 'new'
+        else 
+            session[:name] = params[:name]
+            redirect_to '/'
+        end         
+    end   
+    
+    def destroy 
+        session.delete :name 
+        redirect_to '/'
+    end     
 
-  def destroy
-    session.delete :name
-    redirect_to root_path
-  end
-end
+end     

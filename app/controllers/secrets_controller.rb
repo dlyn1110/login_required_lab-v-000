41f65d4e73851dc07cf6c03b1b8b751549ec73f7
Dani Lyn <dlyn1110@gmail.com>
2019-01-11 03:31:15 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,6 @@
 class SecretsController < ActionController::Base
-   
+   before_action :require_login
+   skip_before_action :require_login, only: [:index]
    def show
      if !current_user
        redirect_to '/login'
